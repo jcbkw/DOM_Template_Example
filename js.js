0a4bddc0b854ebbb78950b5d5b6c7a1997fbae22
Homemade Codes <Homemade Codes>
2017-05-29 11:41:32 -0400

    Refactored external html_templates

--- js.js
+++ js.js
@@ -1,42 +1,62 @@
 $(function () {
     
-    var templates = $('[templates]').remove();
-    
-    function renderPage () {
+    function renderPage (container, content, layoutHtml) {
         
-        templates.find('.mh-title').text('Too cool');
-        templates.find('.main-content').text('For school');
+        container.append(layoutHtml);
         
-        $('body').append(templates.find('[page]').html());
+        container.find('.mh-title').text(content.title);
+        container.find('.main-content').prepend(content.subtitle);
         
     }
     
-    function renderList () {
+    function renderList (container, data, contactHtml) {
         
-        $.get('data.json', function (data) {
+        var ul = container.find('.plain-list'),
+            liTpl = $(contactHtml);
+        
+        data.forEach(function (person) {
             
-            var ul = templates.find('.plain-list'),
-                liTpl = templates.find('.pl-item');
+            var li = liTpl.clone();
             
-            data.forEach(function (person) {
-                
-                var li = liTpl.clone();
-                
-                li.find('.col-1').text(person.firstname);
-                li.find('.col-2').text(person.lastname);
-                li.find('.col-3').text(person.phone);
-                
-                ul.append(li);
-                
-            });
+            li.find('.col-1').text(person.firstname);
+            li.find('.col-2').text(person.lastname);
+            li.find('.col-3').text(person.phone);
             
-            $('.main-content').append(ul);
+            ul.append(li);
             
         });
         
     }
     
-    renderPage();
-    renderList();
+    function getServerFiles (callback) {
+        
+        $.get('layout.html', function (layoutHtml) {
+            
+            $.get('contact.html', function (contactHtml) {
+                
+                $.get('data.json', function (data) {
+                    
+                    $.get('content.json', function (content) {
+                    
+                        callback(data, content, layoutHtml, contactHtml);
+                    
+                    });
+                    
+                });
+                
+            });
+            
+        });
+        
+    }
     
+    getServerFiles(function (data, content, layoutHtml, contactHtml) {
+        
+        var body = $('body');
+        
+        renderPage(body, content, layoutHtml);
+        renderList(body, data, contactHtml);
+        
+    });
+        
 });
